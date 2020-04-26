d750d7e9e4f3b6fa44550deeef64074e53be3ed6
P Bridwell <patbridwell@cox.net>
2020-04-26 18:10:34 -0500

    moves queries into seperate file

--- main.js
+++ main.js
@@ -2,27 +2,10 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const queries = require("./queries.js");
 
-var connection = mysql.createConnection({
-  host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "password",
-  database: "personnel"
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connection to database successful " + connection.threadId + "\n");
-  start();
-});
-
+start();
 
 function start() {
     inquirer
@@ -34,39 +17,36 @@ function start() {
               choices: [
                 "View all employees",
                 "Add employee",
-                "Remove Employee",
                 "View departments",
-                "Add departments",
-                "Remove Departments", 
+                "Add departments", 
                 "View roles", 
                 "Update employee roles",
-                "Add roles",
-                "Finish" 
+                "Add roles" 
             ]
           },
         ])
+
         .then(res => {
             console.log(res);
-        })
-          
         // Set switch based on initial answer
-        // const homepage = res.homepage
-        // switch(homepage) {
-        //     case "View all employees": viewAll();
-        //         break;
-        //     case "View employees by roll": viewByRoll();
-        //         break;
-        //     case "View employees by department": viewByDeparment();
-        //         break;
-        //     case "Add a department": addDepartment();
-        //         break;
-        //     case "Add an employee": addEmployee();
-        //         break;
-        //     case "Add a role": addRole();
-        //         break;
-        //     case "Update an employee's role": updateRole();
-        //         break;
-        //     case "Finish": endConnection();
-        //  };
-      };
-  
+        const homepage = res.homepage
+        switch(homepage) {
+            case "View all employees": queries.viewAll();
+                break;
+            // case "View employees by roll": viewByRoll();
+            //     break;
+            // case "View employees by department": viewByDeparment();
+            //     break;
+            // case "Add a department": addDepartment();
+            //     break;
+            // case "Add an employee": addEmployee();
+            //     break;
+            // case "Add a role": addRole();
+            //     break;
+            // case "Update an employee's role": updateRole();
+            //     break;
+            // case "Finish": endConnection();
+                default: connection.end()
+         };
+    })
+};
